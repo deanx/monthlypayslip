7781a300c441ea22a39bbb9e4ae4394a805440b7
Alex Costa <alex@deanx.com.br>
2016-03-28 00:37:32 +1100

    run tests

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var EventEmitter = require('events');
 var program = require('commander');
+var fs = require('fs');
 
 program.version('1.0.0')
- .option('-f --file', 'CSV file')
+ .option('-f --file [file]', 'CSV file')
  .parse(process.argv);
 
 if(!program.file) {
   throw(new Error('will must inform the CSV file to be parsed'));
+}
+
+if(!fs.exists(program.file)) {
+  throw(new Error('file does not exist'));
 }
--- test/app.js
+++ test/app.js
@@ -1,26 +1,75 @@
+var fs = require('fs');
 var assert = require('chai').assert;
 var exec = require('child_process');
+var lineByLineReader = require('line-by-line');
 
 describe('App', function() {
-  describe('Run', function() {
-    var error, commandOut, commandErr;
+  var outputFile = './output.csv';
+  var error, commandOut, commandErr;
+  var commandReturnParseWithNullReturn = function(err, stdout, stderr,done) {
+    error = err;
+    commandOut = stdout;
+    commandErr = stderr;
+    done(null);
+  };
+  describe('Run without file', function() {
     before(function (done) {
       var commandLine = 'node ' + appRelativePath + appIndexFile;
-      try {
         exec.exec(commandLine, function(err, stdout, stderr) {
-	         error = err;
-           commandOut = stdout;
-           commandErr = stderr;
-           done(null);
+	         commandReturnParseWithNullReturn(err, stdout, stderr, done);
          });
-       } catch (err) {
-         console.log('erroooom');
-         done(null);
-       }
     });
 
     it('should not execute  without a file name', function() {
       assert.instanceOf(error, Error);
+    });
+  });
+
+  describe('Run with non-existent file', function() {
+    before(function(done) {
+      var commandLine = 'node ' + appRelativePath + appIndexFile + '--file=./test/non-existent-test.csv';
+      exec.exec(commandLine, function(err, stdout, stderr) {
+        commandReturnParseWithNullReturn(err, stdout, stderr, done);
+      });
+    });
+
+    it('should raise an error with the non-existent file', function() {
+      assert.instanceOf(error, Error);
+    });
+  });
+
+  describe('Run with existent file', function() {
+    before(function (done) {
+      try {
+        fs.unlinkSync(outputFile);
+      } catch(err) {
+        // file still do not exists. No problem!
+      }
+
+      var commandLine = 'node ' + appRelativePath + appIndexFile + '--file=./test/empty-file.csv';
+      exec.exec(commandLine, function(err, stdout, stderr) {
+        commandReturnParseWithNullReturn(err, stdout, stderr, done);
+      });
+    });
+    it('should create a new file for correct calls', function() {
+      assert.isTrue(fs.existsSync(outputFile));
+    });
+  });
+
+  describe('Run with existent file but wrong input in the second line', function() {
+    before(function (done) {
+    var commandLine = 'node ' + appRelativePath + appIndexFile + '--file=./test/wrong-input-2-line.csv';
+      exec.exec(commandLine, function(err, stdout, stderr) {
+        var reader = new lineByLineReader(outputFile);
+        var lines = [];
+        reader.on('line', function(line) {
+          lines.push(line);
+        });
+        commandReturnParseWithNullReturn(err, stdout, stderr, done);
+      });
+    });
+    it('Should verify if the second line of generated file has a error message', function() {
+      assert.equal('Wrong data', lines[1]);
     });
   });
 });
